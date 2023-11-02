607db5c6e2b45dd639e47b179355c22def625253
rodmjay <rodmjay@gmail.com>
2023-11-02 07:26:57 -0600

    add application languages

---
+++ src/TranslationPro.App/Pages/LanguageList.razor.cs
@@ -0,0 +1,13 @@
+﻿using TranslationPro.App.Pages.Bases;
+using TranslationPro.Shared.Models;
+
+namespace TranslationPro.App.Pages
+{
+    public partial class LanguageList : ApplicationDetailsBase
+    {
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
